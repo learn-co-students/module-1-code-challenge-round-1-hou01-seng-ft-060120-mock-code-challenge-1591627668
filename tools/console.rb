b7e4329da346a4fb35d7fdc865e82841b050b63d
jamesjlandry <james.j.landry@outlook.com>
2020-06-08 12:01:06 -0500

    submitted

--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,5 @@
 # require and load the environment file
+require 'pry'
 require_relative '../config/environment.rb'
 
 # call this method to reload your models as you write code
@@ -10,7 +11,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+steve = Tutor.new("Steve")
+josh = Tutor.new("Josh")
+mark = Student.new("Mark")
+ruby = Course.new(steve, mark, "Ruby")
+pyhton = Course.new(josh, mark, "Python")
+javascript = Course.new(josh, mark, "Javascript")
 
+p ruby.student
+p josh.courses
+p josh.students
+p mark.courses
+p mark.tutors
 
 
 
